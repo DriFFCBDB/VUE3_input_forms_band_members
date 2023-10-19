c992e595115e8e77eefcbe5ed2ebdffb01c574b2
DriFFCBDB <adriellibarboza.1105@aluno.saojudas.br>
2023-10-19 14:16:16 -0300

    Formulários e Inputs

---
+++ scripts.js
@@ -0,0 +1,37 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Violão'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Guitarra'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members,
+            newMember: {
+    
+            }
+        }
+    },
+    methods: {
+        addMember(){
+            if (this.newMember.fname && this.newMember.lname && this.newMember.instrument) {           
+                this.members.push(this.newMember)
+                this.newMember = {}
+            }else{
+                alert("Todos os campos devem conter dados.")
+            }
+        }
+    },
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
